00e4d6187efda011fe7f0397f8e38c2d3aa2fb04
Sean Curran <scurran3@kent.edu>
2020-04-06 13:26:43 -0400

    Added Search component and detail view, changed auth on home page

---
+++ vue_starter_dotnet/frontend/src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder='Enter your city...' v-model='city' />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            city: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
