96c1838183e4c4f96f0c36ccb694dab9d26d4426
Sean Curran <scurran3@kent.edu>
2020-04-06 16:57:49 -0400

    Added api fetch

--- vue_starter_dotnet/frontend/src/components/Search.vue
+++ vue_starter_dotnet/frontend/src/components/Search.vue
@@ -1,14 +1,38 @@
 <template>
     <div class="search">
         <input type="text" placeholder='Enter your city...' v-model='city' />
+        <button type='submit' @click="findLandmarks">Submit</button>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'Search',
     data() {
         return {
-            city: ''
+            
+            lat: '41.505550',
+            long: '-81.691498',
+            radius: '1500',
+            key: 'AIzaSyANWIg-qW05HeNmXG2Yh1Fd7w8I9w4WXto',
+            results: []
+            //city: ''
+        }
+    },
+    methods: {
+        findLandmarks() {
+            const apiUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+             + this.lat + ',' + this.long + '&radius=' + this.radius + '&key=' + this.key;
+            fetch(apiUrl, {
+                Method: 'GET'
+            })
+                .then( response => {
+                    response.json()
+                    .then( json => {
+                        this.results = json;
+                    })
+                }).catch(err => {console.log(err)});
+              
         }
     }
 }
--- vue_starter_dotnet/frontend/src/views/Home.vue
+++ vue_starter_dotnet/frontend/src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <search></search>
     <p>You must be authenticated to see this</p>
   </div>
 </template>
 
 <script>
+import Search from '@/components/Search.vue'
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    Search
+  }
 }
 </script>
