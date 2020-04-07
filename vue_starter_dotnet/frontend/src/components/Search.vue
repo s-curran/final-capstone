89e739b91a8f81cf445540a1183f246c182db2e1
Sean Curran <scurran3@kent.edu>
2020-04-07 17:14:08 -0400

    Styled search component and finished functionality

--- vue_starter_dotnet/frontend/src/components/Search.vue
+++ vue_starter_dotnet/frontend/src/components/Search.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="search">
-    
+    <!-- <input type="text" placeholder="Enter your city..." v-model="city" />
+    <br />-->
 
-    <input type="text" placeholder="Enter your city..." v-model="city" />
+    <button type="button" @click="getLocation">Allow Access to Your Location</button>
     <br />
-
-    <!-- <button type='submit' @click='getLocation'>Search Near You</button> -->
-
+    <div class="box">
     <label>Distance:</label>
+
     <select v-model="radius">
       <option value="1609.34">1 mile</option>
       <option value="3218.69">2 miles</option>
       <option value="8046.72">5 miles</option>
       <option value="16093.4">10 miles</option>
     </select>
-    <br />
+    </div>
+   
+    <div class="box">
     <label>Type:</label>
     <select v-model="type">
       <option value="tourist_attraction">Tourist Atrraction</option>
@@ -23,27 +25,27 @@
       <option value="zoo">Zoo</option>
       <option value="museum">Museum</option>
     </select>
-    <br />
+    </div>
+   
     <label>Only open now</label>
     <input type="checkbox" v-model="opennow" value="true" />
     <br />
     <button type="submit" @click="findLandmarks">Submit</button>
-    <ul>
+    <!-- <ul>
       <li v-for="result in results" v-bind:key="result.id">{{result.name}}</li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Search",
   data() {
     return {
-    //   lat: '',
-    //   long: '',
-      lat: "41.505550",
-      long: "-81.691498",
+      lat: "",
+      long: "",
+      //   lat: "41.505550",
+      //   long: "-81.691498",
       radius: "",
       type: "",
       opennow: "",
@@ -53,56 +55,25 @@ export default {
     };
   },
   methods: {
-      findLandmarks() {
-      
-        let url = `${process.env.VUE_APP_REMOTE_API}/search/places?location=${this.lat},${this.long}&radius=${this.radius}&type=${this.type}&key=${this.key}`;
-        if (this.opennow) {
-          url += "&opennow=true";
-        }
+    findLandmarks() {
+      let url = `${process.env.VUE_APP_REMOTE_API}/search/places?location=${this.lat},${this.long}&radius=${this.radius}&type=${this.type}&opennow=${this.opennow}`;
 
-        fetch(url)
-          .then(response => {
-            if (response.ok) {
-              response.text().then(txt => {
-                this.results = JSON.parse(txt).results;
-              });
-            } else {
-              console.log(response.status);
-            }
-          })
-          .catch(err => {
-            console.log(err);
-          });
+      fetch(url)
+        .then(response => {
+          if (response.ok) {
+            response.text().then(txt => {
+              this.results = JSON.parse(txt).results;
+            });
+          } else {
+            console.log(response.status);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
 
-          this.$emit('results', this.results);
+      this.$emit("results", this.results);
     },
-    // findLandmarks() {
-    //   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-
-
-    //     let url = proxyurl + `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.lat},${this.long}&radius=${this.radius}&type=${this.type}&key=${this.key}`;
-    //     if (this.opennow) {
-    //       url += "&opennow=true";
-    //     }
-
-    //     fetch(url, {
-    //       headers: {
-    //         "User-Agent": "PostmanRuntime/7.24.0"
-    //       }
-    //     })
-    //       .then(response => {
-    //         if (response.ok) {
-    //           response.json().then(json => {
-    //             this.results = json.results;
-    //           });
-    //         } else {
-    //           console.log(response.status);
-    //         }
-    //       })
-    //       .catch(err => {
-    //         console.log(err);
-    //       });
-    // },
     getLocation() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(this.showPosition);
@@ -119,4 +90,59 @@ export default {
 </script>
 
 <style>
+button {
+  display: inline-block;
+  padding: 0.5em 3em;
+  border: 0.16em solid #515458;
+  margin: 5px 0.3em 0.3em 0;
+  box-sizing: border-box;
+  font-size: 14px;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-family: "Verdana", sans-serif;
+  font-weight: 400;
+  color: #515458;
+  text-align: center;
+  transition: all 0.15s;
+  background-color: white;
+}
+button:hover {
+  color: #03dbfc;
+  border-color: #03dbfc;
+}
+button:active {
+  color: #03dbfc;
+  border-color: #03dbfc;
+}
+label {
+  text-align: left;
+  text-transform: uppercase;
+  font-family: "Verdana", sans-serif;
+  font-weight: 500;
+  color: #515458;
+}
+
+.box {
+    margin: 0 auto 0 auto;
+    display: flex;
+    width: 300px;
+    justify-content: space-between;
+    align-items: center; 
+}
+
+select {
+  display: inline-block;
+  padding: 0.5em 1em;
+  border: 0.16em solid #515458;
+  margin: 5px 0.3em 0.3em 0;
+  box-sizing: border-box;
+  font-size: 14px;
+  text-align-last: right;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-family: "Verdana", sans-serif;
+  font-weight: 400;
+  color: #515458;
+  text-align: center;
+}
 </style>
