18ed966fbeecfb98c75792ba417570f603411dca
Sean Curran <scurran3@kent.edu>
2020-04-11 16:01:45 -0400

    Added functionality to home page

--- vue_starter_dotnet/frontend/src/components/CreateItinerary.vue
+++ vue_starter_dotnet/frontend/src/components/CreateItinerary.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
+    <div class="box">
     <label>Itinenary Name</label>
     <input type="text" v-model="itinerary.TourName" />
+    </div>
+    <div class="box">
     <label>Itinenary Date</label>
     <input type="date" v-model="itinerary.DateOfTour" />
+    </div><div class="box">
     <label>Starting Address</label>
     <input type="text" v-model="itinerary.StartPoint" />
+    </div>
+   
     <button type="button" @click="saveItin">Create</button>
+    
   </div>
 </template>
 
@@ -55,5 +62,61 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+button {
+  display: inline-block;
+  padding: 0.5em 3em;
+  border: 0.16em solid #515458;
+  margin: 5px 0.3em 0.3em 0;
+  box-sizing: border-box;
+  font-size: 14px;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-family: "Verdana", sans-serif;
+  font-weight: 400;
+  color: #515458;
+  text-align: center;
+  transition: all 0.15s;
+  background-color: white;
+
+}
+button:hover {
+  color: #03dbfc;
+  border-color: #03dbfc;
+}
+button:active {
+  color: #03dbfc;
+  border-color: #03dbfc;
+}
+label {
+  text-align: left;
+  text-transform: uppercase;
+  font-family: "Verdana", sans-serif;
+  font-weight: 500;
+  color: #515458;
+}
+
+.box {
+  margin: 0 auto 0 auto;
+  display: flex;
+  width: 400px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+input {
+  display: inline-block;
+  padding: 0.5em 1em;
+  border: 0.16em solid #515458;
+  margin: 5px 0.3em 0.3em 0;
+  box-sizing: border-box;
+  font-size: 14px;
+  text-align-last: right;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-family: "Verdana", sans-serif;
+  font-weight: 400;
+  color: #515458;
+  text-align: center;
+}
 </style>
--- vue_starter_dotnet/frontend/src/views/Home.vue
+++ vue_starter_dotnet/frontend/src/views/Home.vue
@@ -2,84 +2,147 @@
   <div class="home">
     <!-- <search class="search" ></search> -->
     <search class="search" v-on:results="displayResults" v-model="searchResults"></search>
-<create-itin></create-itin>
-    <div v-if="searchResults.length >=1">
-        <h3 class="listTitle">Landmarks to visit {{cityName}}:</h3>
-      <ul>
-        <li v-for="result in searchResults" v-bind:key="result" class="result">
-          <router-link :to="{ name: 'detail', params: { id: result.place_id }}">{{result.name}}</router-link>
-         <!-- <a v-bind:href="`{{result.name}}`">{{result.name}}</a> -->
-        </li>
-      </ul>
+    <br />
+    <div v-if="user" class="itin">
+      <div v-if="toggle === false" class="button">
+        <button type="button" @click="toggleForm">Create Itinerary</button>
       </div>
       <div v-else>
-        <h3 class="needToSearch">Enter a city above to view landmarks!</h3>
-     </div>
+        <create-itin class="create"></create-itin>
+      </div>
+      <p>-OR-</p>
+      <select-itin @selected="handleEvent" class="selectItin"></select-itin>
+    </div>
+    <div v-if="searchResults.length >=1">
+      <h3 class="listTitle">Landmarks to visit {{cityName}}:</h3>
+      <ul>
+        <li v-for="result in searchResults" v-bind:key="result" class="result">
+          <router-link :to="{ name: 'detail', params: { id: result.place_id }}" class="resultText">{{result.name}}</router-link>
+          <div v-if="itineraryId" class="addTo">
+          <add 
+            :LandmarkId="result.place_id"
+            :LandmarkName="result.name"
+            :LandmarkAddress="result.vicinity"
+            :ItineraryId="itineraryId"
+          ></add>
+          </div>
+          <!-- <a v-bind:href="`{{result.name}}`">{{result.name}}</a> -->
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <h3 class="needToSearch">Enter a city above to view landmarks!</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import Search from '@/components/Search.vue'
-import CreateItin from '@/components/CreateItinerary.vue'
+import Search from "@/components/Search.vue";
+import CreateItin from "@/components/CreateItinerary.vue";
+import SelectItin from "@/components/SelectItinerary.vue";
+import auth from "../auth";
+import Add from "@/components/AddToItinerary.vue"
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     Search,
     CreateItin,
+    SelectItin,
+    Add
   },
-  data(){
-    return{
-    cityName: "near you",
-
-    searchResults: [
-    // { id: "3cf5baf562b322d3bd794748a971263f6af071df", name: "Nautica Queen Cleveland's Dining Cruise Ship"},
-    // { id: "6edc2f98f10c2d0137ecc4c135db3760605df971", name: "Shooters On the Water"},
-    // { id: "f37878763520ba80ccd8fc4772137754d63c1c86", name: "Lago East Bank"},
-    // { id: "7c9160b5e479779b9b1b3ecc6b72f4c3703c30df", name: "Alley Cat Oyster Bar"},
-  ]
+  data() {
+    return {
+      cityName: "near you",
+      itineraryId: "",
+      user: null,
+      toggle: false,
+      searchResults: [
+        // { id: "3cf5baf562b322d3bd794748a971263f6af071df", name: "Nautica Queen Cleveland's Dining Cruise Ship"},
+        // { id: "6edc2f98f10c2d0137ecc4c135db3760605df971", name: "Shooters On the Water"},
+        // { id: "f37878763520ba80ccd8fc4772137754d63c1c86", name: "Lago East Bank"},
+        // { id: "7c9160b5e479779b9b1b3ecc6b72f4c3703c30df", name: "Alley Cat Oyster Bar"},
+      ]
     };
   },
   methods: {
-    displayResults(results)
-    {
+    displayResults(results) {
       this.searchResults = results;
+    },
+    handleEvent(selected) {
+      this.itineraryId = selected;
+    },
+    toggleForm() {
+      this.toggle = true;
+    }
+  },
+  created() {
+    this.user = auth.getUser();
+  },
+  watch: {
+    $route: function() {
+      this.user = auth.getUser();
     }
   }
-
-}
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Lobster&family=Pacifico&display=swap');
-.pageTitle{
-  font-family: 'Lobster', cursive;
-      font-size: 55px;
-
+@import url("https://fonts.googleapis.com/css2?family=Lobster&family=Pacifico&display=swap");
+.pageTitle {
+  font-family: "Lobster", cursive;
+  font-size: 55px;
 }
-.listTitle{
-    font-family: verdana;
-    font-size: 25px;
-    text-align: center;
-    margin-top: 20px;
-    margin-bottom: 20px;
+.listTitle {
+  font-family: verdana;
+  font-size: 25px;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
-.search{
+p {
+  font-family: Verdana, sans-serif;
+  text-align: center;
+  color: #515458;
+}
+.search {
   text-align: center;
 }
-.result{
-    font-family: verdana;
-    font-size: 18px;
-    text-align: center;
-    list-style-type: none;
-    margin-top: 20px;
-    margin-bottom: 20px;
+.create {
+  text-align: center;
 }
-.needToSearch{
-    font-family: verdana;
-    font-size: 25px;
-    text-align: center;
-    margin-top: 20px;
-    margin-bottom: 20px;
+.selectItin {
+  text-align: center;
+}
+.result {
+  font-family: verdana;
+  font-size: 18px;
+  text-align: center;
+  list-style-type: none;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.needToSearch {
+  font-family: verdana;
+  font-size: 25px;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.button {
+  text-align: center;
+  margin: auto;
+}
+.addTo {
+  display: inline;
+}
+a {
+  display: inline;
+  text-decoration: none;
+  text-transform: uppercase;
+  color:#515458;
+}
+a:hover {
+  color:#03dbfc;
 }
 </style>
