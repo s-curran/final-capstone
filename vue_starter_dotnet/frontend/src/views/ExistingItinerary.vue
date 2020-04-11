033101c29fe6e38412e9662339c1fb71f5a60cdd
Katie Martin <katieadalinemartin@gmail.com>
2020-04-11 16:00:19 -0400

    Delete landmark functionality

--- vue_starter_dotnet/frontend/src/views/ExistingItinerary.vue
+++ vue_starter_dotnet/frontend/src/views/ExistingItinerary.vue
@@ -4,19 +4,42 @@
     <h1>{{itinerary.tourName}}</h1>
     <p>{{itinerary.dateOfTour}}</p>
     <div v-for="landmark in itinerary.landmarks" v-bind:key="landmark.landmarkId">
-        <p>{{landmark.landmarkName}}</p>
+        <p>{{landmark.landmarkName}}</p> <button v-on:click="handleEvent(landmark.landmarkId)">Delete {{landmark.landmarkName}}from itinerary</button>
     </div>
+    <!-- <Search></Search> -->
+    <!-- <add :LandmarkId="landmark.place_id" :ItineraryId="itineraryId"></add> -->
+
     <!-- need button for edit itinerary and delete itinerary -->
   </div>
 </template>
 
 <script>
 import auth from '../auth'
+import Vue from 'vue';
+
+// import Add from '@/components/AddToItinerary.vue'
+// import Search from '@/components/Search.vue'
+
 
 
 export default {
   name: 'ExistingItinerary',
-  components: {},
+  components: {
+    // Add,
+    // Search
+  },
+    props: {
+      LandmarkId: String,
+      ItineraryId: Number
+  },
+   computed: {
+    item: function() {
+      return {
+        LandmarkId: this.landmarkid,
+        ItineraryId: this.itinerary.itineraryId,
+      };
+    }
+  },
   created(){
       this.getItinerary(this.$route.params.id);
   },
@@ -40,6 +63,30 @@ export default {
     };
   },
   methods: {
+    deleteLandmark() {
+      fetch(`${process.env.VUE_APP_REMOTE_API}/itinerary/remove?itineraryid=${this.ItineraryId}&landmarkid=${this.LandmarkId}`, {
+         method: "DELETE",
+        headers: new Headers({
+          Authorization: "Bearer " + auth.getToken(),
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify(this.item)
+      })
+        .then(response => {
+          if (response.ok) {
+            alert("Landmark has been deleted from itinerary");
+            Vue.$forceUpdate();
+          } else {
+            console.log("Could not delete landmark");
+          }
+        })
+        .catch(err => console.error(err));
+    },
+      handleEvent(id) {
+    this.landmarkid = id;
+    this.deleteLandmark();
+  },
     getItinerary(itineraryid) {      
       fetch(`${process.env.VUE_APP_REMOTE_API}/itinerary/display?itineraryId=${itineraryid}`, {
         method: 'GET',
