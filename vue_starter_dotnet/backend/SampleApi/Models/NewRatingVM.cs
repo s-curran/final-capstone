1ad82f76a6f5bc17beb0307dad1850710708f1fd
Katie Martin <katieadalinemartin@gmail.com>
2020-04-14 14:38:50 -0400

    Save rating to backend

---
+++ vue_starter_dotnet/backend/SampleApi/Models/NewRatingVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Models
+{
+    public class NewRatingVM
+    {
+        public Landmark landmarkVM { get; set; }
+        public int newNumberOfRatings { get; set; }
+
+        public double newAverageRating { get; set; }
+
+    }
+}
